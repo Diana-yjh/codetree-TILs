5ab2941df4b8bcd1ada1d1755ffd43905fa5f0c5
Diana <57698939+Diana-yjh@users.noreply.github.com>
2024-07-09 21:57:41 +0900

    Title: 배열 놀이, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240709/배열 놀이/play-with-array.swift
@@ -0,0 +1,40 @@
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let n = input[0]
+let q = input[1]
+
+let array = readLine()!.split(separator: " ").map{Int($0)!}
+
+func calculate(rpt: Int) {
+    for _ in 0..<rpt {
+        let question = readLine()!.split(separator: " ").map{Int($0)!}
+        let `case` = question[0]
+        switch `case` {
+            case 1:
+                first(index: question[1])
+            case 2:
+                second(b: question[1])
+            case 3:
+                third(s: question[1], e: question[2])
+            default:
+                print("?")
+        }
+    }
+}
+
+func first(index: Int) {
+    print(array[index - 1])
+}
+
+func second(b: Int) {
+    guard let index = array.firstIndex(of: b) else {
+        print(0)
+        return
+    }
+    print(index + 1)
+}
+
+func third(s: Int, e: Int) {
+    array[(s-1)...(e-1)].forEach{item in print(item, terminator: " ")}
+}
+
+calculate(rpt: q)
