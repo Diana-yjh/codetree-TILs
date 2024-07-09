2b10f061e57bdc047178de1a9d3382f663a2e55b
Diana <57698939+Diana-yjh@users.noreply.github.com>
2024-07-09 14:52:45 +0900

    Title: 최소공배수 구하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240709/최소공배수 구하기/find-the-least-common-multiple.swift
+++ 240709/최소공배수 구하기/find-the-least-common-multiple.swift
@@ -1,7 +1,8 @@
 let input = readLine()!.split(separator: " ").map {Int($0)!}
 
 func calculate(_ n: Int, _ m: Int) {
-    let result = Array(1...100).filter { $0 % n == 0 && $0 % m == 0 }
+    var result = [1]
+    result = Array(1...100).filter { $0 % n == 0 && $0 % m == 0 }
     print(result.min()!)
 }
 
