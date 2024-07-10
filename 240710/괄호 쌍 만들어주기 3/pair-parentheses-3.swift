8f6cd2bfe620d8f08703d6cf3e56449be67df091
Diana <57698939+Diana-yjh@users.noreply.github.com>
2024-07-10 14:49:16 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 9ms, Memory: 1MB, Status: Passed - CodeTree

--- 240710/괄호 쌍 만들어주기 3/pair-parentheses-3.swift
+++ 240710/괄호 쌍 만들어주기 3/pair-parentheses-3.swift
@@ -1,21 +1,10 @@
-var array: [String] = []
-var input = readLine()!
+var input = readLine()!.split(separator: "").map {String($0)}
 var result = 0
 
-for char in input {
-    array.append(String(char))
-}
-
-if array.count != 1 {
-    for i in 0..<array.count-1 {
-        if array[i] == "(" {
-            for j in i..<array.count {
-                if array[j] == ")" {
-                    result += 1
-                }    
-            }    
-        }
-    }
+for i in 0..<input.count-1 where input[i] == "("{
+    for j in i..<input.count where input[j] == ")"{
+        result += 1   
+    }    
 }
 
 print(result)
