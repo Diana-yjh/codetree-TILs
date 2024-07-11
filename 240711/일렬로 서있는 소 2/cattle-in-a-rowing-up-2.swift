eacc351b28954e60302af13fc50129fbdd4d69a2
Diana <57698939+Diana-yjh@users.noreply.github.com>
2024-07-11 12:19:32 +0900

    Title: 일렬로 서있는 소 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240711/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
+++ 240711/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
@@ -6,15 +6,14 @@ for i in 0..<n-2 {
     var count = 0
     var min = cows[i]
 
-    for j in i+1..<n {
-        if min <= cows[j] {
-            min = cows[j]
-            count += 1
+    for j in i+1..<n-1 {
+        if cows[i] <= cows[j] {
+            for k in j+1..<n {
+                if cows[j] <= cows[k] {
+                    result += 1
+                }
+            }
         }
-    }
-    
-    if count > 1 {
-        result += 1
     }
 }
 
