5d4b42feb4711a461a77e7c818bd3766beb9cf48
Diana <57698939+Diana-yjh@users.noreply.github.com>
2024-07-11 12:05:39 +0900

    Title: 일렬로 서있는 소 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240711/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
+++ 240711/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
@@ -1,23 +1,21 @@
 let n = Int(readLine()!)!
 let cows = readLine()!.split(separator: " ").map { Int($0)! }
-
-var able = 0
 var result = 0
 
 for i in 0..<n-2 {
+    var count = 0
     var min = cows[i]
+
     for j in i+1..<n {
         if min <= cows[j] {
             min = cows[j]
-            able += 1
+            count += 1
         }
     }
-
-    if able > 2 {
+    
+    if count > 1 {
         result += 1
     }
-
-    able = 0
 }
 
 print(result)
