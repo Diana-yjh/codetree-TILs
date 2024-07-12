e7f2130157350f5fd10fadf63b016a769ef8a288
Diana <57698939+Diana-yjh@users.noreply.github.com>
2024-07-12 13:37:03 +0900

    Title: 바구니 안의 사탕 2, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240712/바구니 안의 사탕 2/candy-in-the-basket-2.swift
@@ -0,0 +1,25 @@
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+
+let n = input[0] // 바구니 개수
+let k = input[1] // 범위
+
+var array = Array(repeating: 0, count: 101)
+var addArray = Array(repeating: 0, count: 101 - n)
+
+for _ in 0..<n {
+    let basket = readLine()!.split(separator: " ").map{Int($0)!}
+    let candy = basket[0]
+    let coordinate = basket[1]
+
+    array[coordinate] = candy
+}
+
+for i in 0...100 - (2 * k) {
+    var add = 0
+    for j in i...i + (2 * k) {
+        add += array[j]
+    }
+    addArray[i] = add
+}
+
+print(addArray.max()!)
