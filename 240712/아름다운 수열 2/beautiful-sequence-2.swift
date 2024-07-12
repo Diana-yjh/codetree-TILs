28738c91085425116829795795bc72a573066022
Diana <57698939+Diana-yjh@users.noreply.github.com>
2024-07-12 12:09:27 +0900

    Title: 아름다운 수열 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240712/아름다운 수열 2/beautiful-sequence-2.swift
@@ -0,0 +1,31 @@
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+
+let n = input[0]
+let m = input[1]
+
+let a = readLine()!.split(separator: " ").map{Int($0)!}
+let b = readLine()!.split(separator: " ").map{Int($0)!}
+
+var result = 0
+
+for i in 0..<n-m+1 {
+    var array = b
+
+    if array.contains(a[i]) {
+        for j in i..<i+m {
+            if array.contains(a[j]) {
+                array.remove(at: getIndex(array: array, item: a[j]))
+            }
+        }
+
+        if array.isEmpty {
+            result += 1
+        }
+    }
+}
+
+print(result)
+
+func getIndex(array: [Int],item: Int) -> Int {
+    return array.firstIndex(of: item)!
+}
